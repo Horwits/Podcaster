3ee9b57bc564752a1a0130c0cafbdec955fd6bd6
studware <angela.teneva@gmail.com>
2017-01-01 20:55:22 +0200

    Added login.module and user-signup files

---
+++ src/app/core/models/user.model.ts
@@ -0,0 +1,10 @@
+export class User {
+    id?: string;
+    username: string;
+    password: string;
+    confirmPassword?: string;
+    email?: string;
+    firstName?: string;
+    lastName?: string;
+    description?: string;
+};
