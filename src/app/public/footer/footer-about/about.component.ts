8b8cc3cf601b14ebe6b10070efd1adfd1b12672d
Daniel Nikolov <horwits1@hotmail.com>
2017-01-02 18:07:50 +0200

    Added styles and html for about

--- src/app/public/footer/footer-about/about.component.ts
+++ src/app/public/footer/footer-about/about.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['../../home/home-main/home-main.component.css']
 })
 export class AboutComponent implements OnInit {
 
