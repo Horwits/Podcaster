7196c751d0e336ccd43237e75ca6f16a86fa508f
Daniel Nikolov <horwits1@hotmail.com>
2017-01-02 20:07:40 +0200

    Added search functionality

--- src/app/public/search/search.component.ts
+++ src/app/public/search/search.component.ts
@@ -4,6 +4,8 @@ import { Observable } from 'rxjs/Observable';
 
 import { HttpService } from '../../core/services/http.service';
 
+import { Podcast } from '../../core/models/podcast.model';
+
 @Component({
     selector: 'app-search',
     templateUrl: './search.component.html',
@@ -11,47 +13,44 @@ import { HttpService } from '../../core/services/http.service';
 })
 
 export class SearchComponent implements OnInit {
-    private items: Array<Object>;
+    private items: Array<Podcast>;
     private errorMessage: string;
+    private apiRootUrl = 'https://itunes.apple.com/search';
 
-    /* private url = 'https://itunes.apple.com/search?term=podcast&country=us&entity=podcast&attribute=ratingIndex&limit=10&callback=JSONP_CALLBACK';*/
-    /*private url = 'https://feedwrangler.net/api/v2/podcasts/get';*/
     private url = 'https://itunes.apple.com/us/rss/toppodcasts/limit=10/explicit=true/json';
     searchTerm: string;
-
-    constructor(private http: Http, private itunesService: HttpService) {
+    private term: string;
+    constructor(private httpService: HttpService) {
     }
-    // Initiate search based on input value
     ngOnInit() {
-        this.getAll();
     }
-    private getAll() {
-        let result;
 
-        this.itunesService.getResultsFromRSS(this.url)
-            .subscribe(
+    private getByTerm(term: string) {
+        let result;
+        let media = 'podcast';
+        return this.httpService.getByTermFromApi(this.apiRootUrl, term, media).subscribe(
             (response) => {
                 result = response;
-                /*console.log(result.results);
-                this.items = result.results.slice(0, 8);*/ // this should be done with a pipe
+                console.log(result);
                 this.items = result
-                    .feed
-                        .entry
-                        .map(x => x = {
-                            author: x["im:name"].label,
-                            image: x["im:image"][2].label,
-                            title: x["title"].label
-                        })
-                        .slice(0, 8);
-                /*console.log(result.feed.entry[0]);*/
+                    .results
+                    .map(item => {
+                        return {
+                            title: item.trackName,
+                            author: item.artistName,
+                            trackViewUrl: item.trackViewUrl,
+                            image: item.artworkUrl600,
+                            feed: item.feedUrl,
+                            genres: item.genres
+                        } as Podcast;
+                    }).slice(0, 8);
             }
             ,
             error => this.errorMessage = error
-            );
+        );
     }
 
-    /*onSearch(searchTerm) {
-        let res = this.items.find(x => x[searchTerm] === this.searchTerm);
-        console.log(res);
-    }*/
+    onSearch(term) {
+        this.getByTerm(term);
+    }
 }
