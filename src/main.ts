fc4f446f40d2c7ba134bfc78375e75b08e50c607
Daniel Nikolov <horwits1@hotmail.com>
2017-01-02 17:48:14 +0200

    Removed itunesService from main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { JsonpModule } from '@angular/http';
-import { ItunesSearchService } from './app/public/search/search.service';
 import { AppModule } from './app/app.module';
 
 if (environment.production) {
