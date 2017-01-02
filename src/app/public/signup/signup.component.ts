d37e2a70c4a8fb94daecb90734b3cdd38a7b5533
Daniel Nikolov <horwits1@hotmail.com>
2017-01-03 01:19:25 +0200

    fixed merging chages

--- src/app/public/signup/signup.component.ts
+++ src/app/public/signup/signup.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from '../../core/models/user.model';
-
+import { Component, OnInit, Injectable } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FirebaseService } from '../../core/services/firebase.service';
+@Injectable()
 @Component({
-    selector: 'app-signup',
+    selector: 'app-register',
     templateUrl: './signup.component.html',
-    styleUrls: ['./signup.component.css']
+    styleUrls: ['./signup.component.css'],
+    providers: [FirebaseService]
 })
 export class SignupComponent implements OnInit {
-    model: User;
+    response;
+    form: FormGroup;
+    constructor(private formBuilder: FormBuilder, private firebaseService: FirebaseService) {
 
-    constructor() { }
+    }
 
-    ngOnInit() { }
+    ngOnInit() {
+        this.form = this.formBuilder.group({
+            username: ['', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(15)])],
+            password: ['', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(15)])],
+            confirmPassword: ['', Validators.required],
+            firstName: ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(20)])],
+            lastName: ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(20)])],
+            email: ['', Validators.required],
+            age: ['', Validators.required],
+            gender: ['', Validators.required]
+        });
+    }
+
+    setUser() {
+        this.firebaseService.setUser(this.form)
+            .subscribe(
+            user => {
+                this.response = JSON.stringify(user);
+                console.log(this.response);
+            },
+            error => console.log(error)
+            );
+    }
+
+    submit(value: any) {
+        console.log(value);
+        this.firebaseService.setUser(value)
+            .subscribe(
+            user => {
+                this.response = JSON.stringify(user);
+                console.log(this.response);
+            },
+            error => console.log(error)
+            );
+		/*this.authService.register(this.form.value)
+			.subscribe(res => {
+				console.log(res);
+			}, err => {
+				console.log(err);
+			});
+	}*/}
 }
+
