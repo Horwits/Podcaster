11b8cb19670aaf74e8f063250d02a90c343ddc35
Daniel Nikolov <horwits1@hotmail.com>
2017-01-03 00:45:57 +0200

    added login module to appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,3 @@
-import { TopEightPodcastsModule } from './public/top-eight-podcasts/top-eight-podcasts.module';
-import { SignupModule } from './public/signup/signup.module';
-import { ProfileModule } from './public/signup/profile/profile.module';
-import { AuthGuard } from './core/services/auth-guard.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -12,6 +8,12 @@ import { FooterModule } from './public/footer/footer.module';
 import { SearchModule } from './public/search/search.module';
 import { QuestionModule } from './public/question/question.module';
 import { HomeModule } from './public/home/home.module';
+import { LoginModule } from './public/login/login.module';
+import { TopEightPodcastsModule } from './public/top-eight-podcasts/top-eight-podcasts.module';
+import { SignupModule } from './public/signup/signup.module';
+import { ProfileModule } from './public/signup/profile/profile.module';
+
+import { AuthGuard } from './core/services/auth-guard.service';
 
 import { AppComponent } from './app.component';
 
@@ -33,7 +35,8 @@ import { AppRoutingModule } from './routes/app-routing.module';
     QuestionModule,
     SignupModule,
     ProfileModule,
-    TopEightPodcastsModule
+    TopEightPodcastsModule,
+    LoginModule
   ],
   declarations: [
     AppComponent,
