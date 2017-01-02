542c8b9c68d0ca11cfe1d577dbe4d6d9bcc11628
studware <angela.teneva@gmail.com>
2017-01-03 00:25:00 +0200

    Add user profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { TopEightPodcastsModule } from './public/top-eight-podcasts/top-eight-podcasts.module';
+import { SignupModule } from './public/signup/signup.module';
+import { ProfileModule } from './public/signup/profile/profile.module';
 import { AuthGuard } from './core/services/auth-guard.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -9,9 +12,6 @@ import { FooterModule } from './public/footer/footer.module';
 import { SearchModule } from './public/search/search.module';
 import { QuestionModule } from './public/question/question.module';
 import { HomeModule } from './public/home/home.module';
-import { LoginModule } from './public/login/login.module';
-import { TopEightPodcastsModule } from './public/top-eight-podcasts/top-eight-podcasts.module';
-import { SignupModule } from './public/signup/signup.module';
 
 import { AppComponent } from './app.component';
 
@@ -32,8 +32,8 @@ import { AppRoutingModule } from './routes/app-routing.module';
     SearchModule,
     QuestionModule,
     SignupModule,
-    TopEightPodcastsModule,
-    LoginModule
+    ProfileModule,
+    TopEightPodcastsModule
   ],
   declarations: [
     AppComponent,
