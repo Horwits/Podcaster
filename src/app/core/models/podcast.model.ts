25d29914448417ff8e35b6e7ecea67ebfbe45fa5
Daniel Nikolov <horwits1@hotmail.com>
2017-01-02 17:46:50 +0200

    Changed podcast model

--- src/app/core/models/podcast.model.ts
+++ src/app/core/models/podcast.model.ts
@@ -1,34 +1,49 @@
 export class Podcast {
-    public wrapperType: string;
-    public kind: string;
-    public collectionId: number;
-    public trackId: number;
-    public artistName: string;
-    public collectionName: string;
-    public trackName: string;
-    public collectionCensoredName: string;
-    public trackCensoredName: string;
-    public collectionViewUrl: string;
-    public feedUrl: string;
-    public trackViewUrl: string;
-    public artworkUrl30: string;
-    public artworkUrl60: string;
-    public artworkUrl100: string;
-    public collectionPrice: string;
-    public trackPrice: number;
-    public trackRentalPrice: number;
-    public collectionHdPrice: number;
-    public trackHdPrice: number;
-    public trackHdRentalPrice: number;
-    public releaseDate: Date;
-    public collectionExplicitness: string;
-    public trackExplicitness: string;
-    public trackCount: number;
-    public country: string;
-    public currency: string;
-    public primaryGenreName: string;
-    public contentAdvisoryRating: string;
-    public artworkUrl600: string;
-    public genreIds: string[];
+    public title: string;
+    public author: string;
+
+    public feed: string;
+
     public genres: string[];
+
+    public image: string;
+    
+    constructor(title, author, image) {
+        this.title = title;
+        this.author = author;
+        this.image = image;
+    }
+
+    /* public wrapperType: string;
+     public kind: string;
+     public collectionId: number;
+     public trackId: number;
+     public artistName: string;
+     public collectionName: string;
+     public trackName: string;
+     public collectionCensoredName: string;
+     public trackCensoredName: string;
+     public collectionViewUrl: string;
+     public feedUrl: string;
+     public trackViewUrl: string;
+     public artworkUrl30: string;
+     public artworkUrl60: string;
+     public artworkUrl100: string;
+     public collectionPrice: string;
+     public trackPrice: number;
+     public trackRentalPrice: number;
+     public collectionHdPrice: number;
+     public trackHdPrice: number;
+     public trackHdRentalPrice: number;
+     public releaseDate: Date;
+     public collectionExplicitness: string;
+     public trackExplicitness: string;
+     public trackCount: number;
+     public country: string;
+     public currency: string;
+     public primaryGenreName: string;
+     public contentAdvisoryRating: string;
+     public artworkUrl600: string;
+     public genreIds: string[];
+     public genres: string[];*/
 }
