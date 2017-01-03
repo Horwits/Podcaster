f09d9299a63fcf3cf22c32ccd1a6df72d0df697e
Daniel Nikolov <horwits1@hotmail.com>
2017-01-03 04:15:03 +0200

    added files for hosting and database connections

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,6 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA5pBjjL9glHx0qwOUkRs6cvoqTYmOJA0o",
+    authDomain: "podcaster-532af.firebaseapp.com",
+    databaseURL: "https://podcaster-532af.firebaseio.com",
+    storageBucket: "podcaster-532af.appspot.com",
+};
